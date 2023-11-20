3f8e5ef15937673b8bdb4c7a4399c415921951c0
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2023-11-20 21:08:27 +0545

    Swapping two variables
    Swapping 2 variables using a temp variable

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+class Swapping
+{
+     public static void main(String [] args)
+     {
+          int a=6;
+          int b=1;
+          System.out.println("a="+a);
+          System.out.println("b="+b);
+          int temp;
+          temp=a;
+          a=b;
+          b=temp;
+          System.out.println("a="+a);
+          System.out.println("b="+b);
+     }
+}
