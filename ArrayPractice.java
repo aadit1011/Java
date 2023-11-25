7a38f6be9592b3110565e833c11e69b5ffe09616
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2023-11-25 19:52:53 +0545

    Simple-Array-Practice
    Simple Array Practice

---
+++ ArrayPractice.java
@@ -0,0 +1,20 @@
+
+import java.util.Arrays;
+
+class ArrayPractice
+{
+     public static void main(String [] args)
+     {
+          int[] numbers=new int[4];
+          numbers[0]=5;
+          numbers[1]=3;
+          numbers[3]=7;
+          System.out.println(Arrays.toString(numbers));
+          String[] names=new String[5];
+          names[0]= "Aadit";
+          names[1]="Arnold";
+          names[2]="Tom";
+          names[3]="Bardan";
+          System.out.println(Arrays.toString(names));
+     }
+}
