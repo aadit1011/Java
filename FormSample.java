0a2bce64bf6173a56d453c968aec2bcfd5827bfe
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2023-12-25 20:11:56 +0545

    Form and Display input on console
    Form Sample and Displaying the result on Console

--- FormSample.java
+++ FormSample.java
@@ -1,13 +1,17 @@
 package CasitaPractice;
 
 import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class FormSample extends JFrame{
-	
+public class FormSample extends JFrame implements ActionListener {
+	JTextField f_name,f_address,f_email;
+	JPasswordField f_password;
 	FormSample(){
 		JFrame frame=new JFrame();
 		frame.setLayout(null);
@@ -26,10 +30,10 @@ public class FormSample extends JFrame{
 		frame.add(address);
 		frame.add(email);
 		frame.add(password);
-		JTextField f_name=new JTextField();
-		JTextField f_address=new JTextField();
-		JTextField f_email=new JTextField();
-		JTextField f_password=new JTextField();
+		 f_name=new JTextField();
+		 f_address=new JTextField();
+		 f_email=new JTextField();
+		 f_password=new JPasswordField();
 		f_name.setBounds(90,30,200,20);
 		f_address.setBounds(90,60,200,20);
 		f_email.setBounds(90,90,200,20);
@@ -41,11 +45,23 @@ public class FormSample extends JFrame{
 		Button submit=new Button("Submit");
 		frame.add(submit);
 		submit.setBounds(100,200,80,20);
+		submit.addActionListener(this);
+		
 	}
-	
 	public static void main(String[] args) {
 		
 		new FormSample();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.dispose();
+		System.out.println("Name is "+f_name.getText());
+		System.out.println("Address is "+f_address.getText());
+		System.out.println("Email is "+f_email.getText());
+		char[] pass=f_password.getPassword();
+		String passSt=new String(pass);
+		System.out.println("Password is "+passSt);
+		System.out.println("You have successfully submitted the form.......");
 	}
 
 }
