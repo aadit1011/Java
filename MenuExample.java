97be718136d36a5f3b7a2ac9b11b63b519faa64c
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2024-01-20 20:53:34 +0545

    Creating Simple MenuBar Using JMenuBar
    Creating Simple MenuBar Using JMenuBar

--- MenuExample.java
+++ MenuExample.java
@@ -1,54 +1,53 @@
+package com.labPractice;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class NotepadFrame {
-	JMenu edit,format,view,help;
-	JMenuItem New,open,save,saveAs,print,exit,cut,copy,paste,delete,undo,replace,view_help,about;
-	
-	NotepadFrame(){
-		JTextArea text=new  JTextArea();
-		text.setBounds(5,5,400,400);
-		JFrame frame=new JFrame("Notepad");
+public class MenuExample {
+	JMenu email,address,roll,number;
+	JMenuItem em,ad,rln,num;
+	MenuExample(){
+		JFrame frame=new JFrame("MenuExample");
 		JMenuBar bar=new JMenuBar();
-		JMenu file=new JMenu("File");
-		format=new JMenu("Format");
-		frame.add(text);
-		view=new JMenu("View");
-		help=new JMenu("Help");
-		New=new JMenuItem("New File");
-		open=new JMenuItem("Open File");
-		save=new JMenuItem("Save");
-		saveAs=new JMenuItem("Save As");
-		print=new JMenuItem("Print");
-		exit=new JMenuItem("Exit");
-		file.add(New);file.add(open);file.add(save);file.add(saveAs);file.add(print);file.add(exit);
-		bar.add(file);
-		edit=new JMenu("Edit");
-		cut=new JMenuItem("Cut");
-		copy=new JMenuItem("Copy");
-		paste=new JMenuItem("Paste");
-		delete=new JMenuItem("Delete");
-		undo=new JMenuItem("Undo");
-		replace=new JMenuItem("Replace");
-		edit.add(cut);	edit.add(copy);	edit.add(paste);edit.add(delete);edit.add(undo);edit.add(replace);
-		bar.add(edit);
-		view=new JMenu("View");
-		view_help=new JMenuItem("View Help");
-		view.add(view_help);
-		help=new JMenu("Help");
-		about=new JMenuItem("About");
-		help.add(about);
-		bar.add(view); bar.add(help);
+		JMenu name=new JMenu("Aadit");
+		JMenuItem name2=new JMenuItem("Bipin");
+		email=new JMenu("Email");
+		address=new JMenu("Address");
+		roll=new JMenu("Roll No.");
+		number=new JMenu("Phone Number");
+		em=new JMenuItem("vardanshiwakoti123@gmail.com");
+		ad=new JMenuItem("Pepsicola");
+		rln=new JMenuItem("210108");
+		num=new JMenuItem("9863034803");
+		email.add(em);
+		address.add(ad);
+		roll.add(rln);
+		number.add(num);
+		name.add(email);
+		name.add(address);
+		name.add(roll);
+		name.add(number);
+		bar.add(name);
+		bar.add(name2);
 		frame.setJMenuBar(bar);
-		frame.setLayout(new BorderLayout());
-		frame.add(text,BorderLayout.CENTER);
+//      to insert horizontal and vertical gaps
+//		frame.setLayout(new BorderLayout(20,20));
+		Button north=new Button("Top");
+		frame.add(north,BorderLayout.NORTH);
+		Button east=new Button("Right");
+		frame.add(east,BorderLayout.EAST);
+		Button west=new Button("Left");
+		frame.add(west,BorderLayout.WEST);
+		Button south=new Button("Bottom");
+		Button center=new Button("Middle");
+		frame.add(center,BorderLayout.CENTER);
+		frame.add(south,BorderLayout.SOUTH);
 		frame.setSize(500,500);
 		frame.setVisible(true);
+		
 	}
 	public static void main(String[] args) {
-		new NotepadFrame();
-
+		new MenuExample();
 	}
 
 }
