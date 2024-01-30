79e8f5c37de71b6edf02f9080eee211fe38840f0
Aadit Sharma <106479910+vardan1010@users.noreply.github.com>
2024-01-30 21:15:42 +0545

    Smiley.java

--- Smily.java
+++ Smily.java
@@ -1,23 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Smily extends Canvas {
+public class Smiley extends Canvas {
 	
 	public void paint(Graphics g) {
 		setBackground(Color.WHITE);
 
+		
 		g.setColor(Color.BLACK);
 		g.drawOval(20, 30, 200, 200);
 		g.setColor(Color.YELLOW);
 		g.fillOval(23,33,197,197);
 		g.setColor(Color.BLACK);
 		g.drawOval(55, 100, 20, 20);
-		g.setColor(Color.yellow);
-		g.fillOval(58, 103, 17, 17);
+		g.setColor(Color.white);
+		g.fillOval(56, 103, 17, 17);
 		g.setColor(Color.BLACK);
 		g.drawOval(155, 100, 20, 20);
-		g.setColor(Color.YELLOW);
-		g.fillOval(158, 103, 17, 17);
+		g.setColor(Color.white);
+		g.fillOval(156, 103, 17, 17);
 		g.setColor(Color.BLACK);
 		g.drawArc(65, 150, 103, 30, 180, 360);
 		g.setColor(Color.WHITE);
@@ -25,10 +26,10 @@ public class Smily extends Canvas {
 
 	}
 	public static void main(String[] args) {
-		Smily exam=new Smily();
+		Smiley smile=new Smiley();
 		
 		JFrame frame=new JFrame("Smily");
-		frame.add(exam);
+		frame.add(smile);
 		frame.setSize(400,500);
 		frame.setVisible(true);
 		frame.setLayout(null);
